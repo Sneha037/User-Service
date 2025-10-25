b21937dc6333b9e0324babc1eba7d62a0b815aeb
Sneha Mukherjee <mukherjeesneha037@gmail.com>
2025-10-26 05:01:08 +0530

    Implemented JWT validation on application layer. Token not persisted

--- src/main/java/org/example/userservice/Configuration/ApplicationConfig.java
+++ src/main/java/org/example/userservice/Configuration/ApplicationConfig.java
@@ -1,11 +1,16 @@
 package org.example.userservice.Configuration;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.MacAlgorithm;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.rsocket.RSocketSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.crypto.Mac;
+import javax.crypto.SecretKey;
 
 @Configuration
 public class ApplicationConfig
@@ -32,5 +37,13 @@ public class ApplicationConfig
         );
 
         return http.build();
+    }
+
+    @Bean
+    public SecretKey secretKey()
+    {
+        MacAlgorithm algorithm = Jwts.SIG.HS256;
+        SecretKey secretKey = algorithm.key().build();
+        return secretKey;
     }
 }
--- src/main/java/org/example/userservice/Services/UserServiceImpl.java
+++ src/main/java/org/example/userservice/Services/UserServiceImpl.java
@@ -1,6 +1,9 @@
 package org.example.userservice.Services;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtParser;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.MacAlgorithm;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.example.userservice.Exceptions.InvalidTokenException;
 import org.example.userservice.Exceptions.PasswordMismatchException;
@@ -13,10 +16,9 @@ import org.example.userservice.Repositories.UserRepository;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.Optional;
+import java.util.*;
 
 @Service
 public class UserServiceImpl implements UserService
@@ -26,13 +28,15 @@ public class UserServiceImpl implements UserService
     BCryptPasswordEncoder bCryptPasswordEncoder;
 
     RoleRepository roleRepository;
+    SecretKey secretKey;
 
-    public UserServiceImpl(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder, TokenRepository tokenRepository, RoleRepository roleRepository)
+    public UserServiceImpl(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder, TokenRepository tokenRepository, RoleRepository roleRepository, SecretKey secretKey)
     {
         this.userRepository = userRepository;
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
         this.tokenRepository = tokenRepository;
         this.roleRepository = roleRepository;
+        this.secretKey = secretKey;
     }
     @Override
     public User signup(String name, String email, String password)
@@ -85,12 +89,14 @@ public class UserServiceImpl implements UserService
 
         token.setUser(user);
         token.setTokenValue(RandomStringUtils.randomAlphanumeric(128));
+      */
 
         Calendar calendar = Calendar.getInstance();
         calendar.add(Calendar.DAY_OF_MONTH, 30); // Token valid for 30 days
-        Date date = calendar.getTime();
-        token.setExpiryDate(date);
+        Date expiryDate = calendar.getTime();
 
+        /*
+        token.setExpiryDate(date);
         return tokenRepository.save(token);
 
        */
@@ -103,7 +109,10 @@ public class UserServiceImpl implements UserService
         // Payload: Claims (user information, expiry date, issued at, etc)
         // Signature
 
-        String userData = "{\n" +
+        /*
+        We won't hardcode the payload in real world application, instead we'll use user information to create the
+         */
+       /* String userData = "{\n" +
                 "   \"email\": \"mukherjeesneha037@gmail.com\",\n" +
                 "   \"roles\": [\n" +
                 "      \"Guest\",\n" +
@@ -116,31 +125,78 @@ public class UserServiceImpl implements UserService
             the payload is the part of the token that contains the claims or information about the user.
             JJWT accepts the payload as a byte array to allow for flexibility in encoding and decoding the data.
            */
-        byte[] payload = userData.getBytes(StandardCharsets.UTF_8);
+
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("userId", user.getId());
+        claims.put("email", user.getEmail());
+        claims.put("expiryDate", expiryDate);
+
+       /* MacAlgorithm algorithm = Jwts.SIG.HS256;
+        SecretKey secretKey = algorithm.key().build();
+        */
+
+        String jwtToken = Jwts.builder()
+                .claims(claims)
+                .signWith(secretKey)
+                .compact();
+
+        return jwtToken;
+
+        //byte[] payload = userData.getBytes(StandardCharsets.UTF_8);
 
         /*
         Using builder pattern here to create JWT token
          */
-        String token = Jwts.builder().content(payload).compact();
+      /*  String token = Jwts.builder().content(payload).compact();
 
         return token;
+       */
     }
 
     @Override
     public User validateToken(String tokenValue) throws InvalidTokenException
     {
-        Optional<Token> optionalToken = tokenRepository.findByTokenValueAndExpiryDateAfter(tokenValue, new Date());
+        //Optional<Token> optionalToken = tokenRepository.findByTokenValueAndExpiryDateAfter(tokenValue, new Date());
 
         /*
         Modified the method to not throw exception, but return null if token is invalid, so that Product Service handles it gracefully
          */
 
-        if(optionalToken.isEmpty())
-        {
+        //if(optionalToken.isEmpty())
+        //{
             //throw new InvalidTokenException("The token is invalid or has expired");
-            return null;
+           // return null;
+        //}
+
+       // return optionalToken.get().getUser();
+
+        /*MacAlgorithm algorithm = Jwts.SIG.HS256;
+        SecretKey secretKey = algorithm.key().build();
+        */
+
+        JwtParser jwtParser = Jwts.parser().verifyWith(secretKey).build();
+
+        Claims claims = jwtParser.parseSignedClaims(tokenValue).getPayload();
+
+        // JWT stores expiry date in Epoch time format
+
+        Date expiryDate = claims.get("expiryDate", Date.class);
+        Date currentDate = new Date();
+
+        if(expiryDate.after(currentDate))
+        {
+            Long userId = claims.get("userId", Long.class);
+
+            Optional<User> optionaluser = userRepository.findById(userId);
+
+            if(optionaluser.isEmpty())
+            {
+                return null;
+            }
+
+            return optionaluser.get();
         }
 
-        return optionalToken.get().getUser();
+        return null;
     }
 }
